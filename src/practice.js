21b39ac69ba41bc96aa9c238df8d829d558273e4
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-13 15:59:49 -0700

    Complete practice #2

--- src/practice.js
+++ src/practice.js
@@ -1,11 +1,18 @@
-function title(name) {
-  return function(title) {
-    return `Hello, ${title} ${name}!`
+// function title(name) {
+//   return function(title) {
+//     return `Hello, ${title} ${name}!`
+//   }
+// }
+// const hello = title("Brandan");
+// console.log(hello("Mr."));
+// console.log(hello("Dr."));
+
+function soundMaker(sound) {
+  return function(animal) {
+    return `The ${animal} goes ${sound}`;
   }
 }
-
-const hello = title("Brandan");
-
-console.log(hello("Mr."));
-
-console.log(hello("Dr."));
+const lionSound = soundMaker("roar");
+const mouseSound = soundMaker("squeak");
+console.log(lionSound("lion"));
+console.log(mouseSound("mouse"));
