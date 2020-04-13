ef4633aa1348c0d879aa29f83f69bbfa10617b25
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-13 16:07:05 -0700

    Complete practice #3

--- src/practice.js
+++ src/practice.js
@@ -7,12 +7,26 @@
 // console.log(hello("Mr."));
 // console.log(hello("Dr."));
 
-function soundMaker(sound) {
-  return function(animal) {
-    return `The ${animal} goes ${sound}`;
+// function soundMaker(sound) {
+//   return function(animal) {
+//     return `The ${animal} goes ${sound}`;
+//   }
+// }
+// const lionSound = soundMaker("roar");
+// const mouseSound = soundMaker("squeak");
+// console.log(lionSound("lion"));
+// console.log(mouseSound("mouse"));
+
+function nameEnhancer(prefix) {
+  return function(suffix) {
+    return function(name) {
+      return `${prefix} ${name} ${suffix}`
+    }
   }
 }
-const lionSound = soundMaker("roar");
-const mouseSound = soundMaker("squeak");
-console.log(lionSound("lion"));
-console.log(mouseSound("mouse"));
+
+const misterJunior = nameEnhancer("Mister")("Junior");
+const duchessThird = nameEnhancer("Duchess")("The Third");
+
+console.log(misterJunior("Jamie"));
+console.log(duchessThird("Jamie"));
