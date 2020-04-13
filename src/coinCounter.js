5bea5bd942eccb873560c8fe54ce1381e4c2b39f
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-13 15:36:21 -0700

    Add findPrimes function

--- src/coinCounter.js
+++ src/coinCounter.js
@@ -1,4 +1,4 @@
-export function coinCounter(totalCost, numCoins = [0,0,0,0], counter = 0) {
+ function coinCounter(totalCost, numCoins = [0,0,0,0], counter = 0) {
   const coinValues = [25, 10, 5, 1];
   if(counter >= 4) {
     return numCoins;
@@ -9,8 +9,41 @@ export function coinCounter(totalCost, numCoins = [0,0,0,0], counter = 0) {
   }
 }
 
-export function closureCounter(totalCost) {
+ function closureCounter(totalCost) {
   return denomination => {
     return (Math.floor(totalCost / denomination));
   };
 }
+
+
+
+
+
+
+ function findPrimes(inputNumber, counter = 2, primeArray = []) {
+  if (isNaN(inputNumber)) {
+    return;
+  }
+  else if(counter > inputNumber) {
+    return primeArray;
+  }
+  else {  
+    let check = true;
+    
+    // check if prime #
+    for(let i = 2; i < counter; i++) {
+      if (counter % i === 0) {
+        check = false;
+        break;
+      }
+    }
+
+    if(check) {
+      primeArray = [...primeArray, counter];
+    }
+
+    return findPrimes(inputNumber, counter + 1, primeArray);
+  }
+}
+
+console.log(findPrimes(541));
