6b6941723c8f6c527842206c877f70c321ce3b36
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-13 10:02:22 -0700

    Add loop logic for coinCounter()

--- src/coinCounter.js
+++ src/coinCounter.js
@@ -1,4 +1,21 @@
 export function coinCounter(totalCost) {
-  
-  return {quarters: 19};
+  if(isNaN(totalCost)) {
+    return;
+  } else if(totalCost <= 0) {
+    return change;
+  }
+
+  const change = {
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0
+  }
+
+
+  return change;
 }
+
+
+const denominations = [.25, .1, .05];
+
